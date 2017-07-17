509f49378aea290ade2dd8f72f56824d1c05ace7
Ilias IRHBOULA <iirhboula@rs2i.fr>
2017-07-17 16:47:38 +0200

    add dto

--- src/main/java/com/firstapp/controllers/RecipeController.java
+++ src/main/java/com/firstapp/controllers/RecipeController.java
@@ -1,7 +1,10 @@
 package com.firstapp.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.firstapp.Models.Recipe;
+import com.firstapp.dto.RecipeDto;
 import com.firstapp.repositories.RecipeJpaRepository;
 
 @RestController
@@ -21,10 +25,14 @@ public class RecipeController {
 	@Autowired
 	private RecipeJpaRepository recipeJpaRepository;
 	
+	private ModelMapper modelMapper = new ModelMapper();
+	
 	@CrossOrigin
 	@GetMapping(value = "/all")
-	public List<Recipe> getRecipes(){
-		return recipeJpaRepository.findAll();
+	public List<RecipeDto> getRecipes(){
+		List<Recipe> recipes = recipeJpaRepository.findAll();
+		return recipes.stream().map(elt -> convertToDto(elt)).collect(Collectors.toList());
+				
 	}
 	
 	@GetMapping(value = "/{name}")
@@ -44,6 +52,20 @@ public class RecipeController {
 
 	public void setRecipeJpaRepository(RecipeJpaRepository recipeJpaRepository) {
 		this.recipeJpaRepository = recipeJpaRepository;
+	}
+	
+	private  RecipeDto convertToDto(Recipe recipe){
+		RecipeDto recipeDto = modelMapper.map(recipe, RecipeDto.class);
+		return recipeDto;
+	}
+	
+	public static void main(String [] args){
+		/*List<Recipe> recipes = new ArrayList<Recipe>();
+		recipes.add(new Recipe(1l,"hh","kk","ll"));
+		//Recipe recipe = new Recipe(1l,"hh","kk","ll");
+	    //System.out.println(RecipeController.convertToDto(recipe).getName());
+		List<RecipeDto> recips = recipes.stream().map(elt -> RecipeController.convertToDto(elt)).collect(Collectors.toList());
+		System.out.println(recips.get(0).getName());*/
 	}
 
 }
---
+++ src/main/java/com/firstapp/dto/RecipeDto.java
@@ -0,0 +1,35 @@
+package com.firstapp.dto;
+
+public class RecipeDto {
+	
+	private Long id;
+	private String name;
+	private String description;
+	private String imagePath;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+}
